c303ab08527ee34bd7e654abc1528fe2f08309dc
Guilherme Novo <guilherme.novo@novoux.com>
2023-01-05 22:35:36 -0300

    Navbar and Footer Development

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
-import '../styles/globals.css'
+import { ThemeProvider } from 'next-themes';
+import Script from 'next/script';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import { NavBar, Footer } from '../components';
+import '../styles/globals.css';
+
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <NavBar />
+      <Component {...pageProps} />
+      <Footer />
+
+    </div>
+
+    <Script src="https://kit.fontawesome.com/69a3d36500.js" crossorigin="anonymous" />
+  </ThemeProvider>
+
+);
+
+export default MyApp;
