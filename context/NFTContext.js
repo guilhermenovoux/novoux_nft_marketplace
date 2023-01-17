d5cff1ebbf3e77af23eb2e908142a21322cb5bab
Guilherme Novo <guilherme.novo@novoux.com>
2023-01-17 11:45:28 -0300

    update 6

--- context/NFTContext.js
+++ context/NFTContext.js
@@ -14,7 +14,7 @@ console.log(projectSecret)
 const auth = `Basic ${Buffer.from(`${projectId}:${projectSecret}`).toString('base64')}`;
 
 
-const options = { host: 'ipfs.infura.io', protocol: 'https', port: 5001, headers: { authorization: auth } };
+const options = { host: 'novoux.ipfs.infura.io', protocol: 'https', port: 5001, headers: { authorization: auth } };
 const client = ipfsHttpClient(options);
 const dedicatedEndPoint = 'https://novoux.infura-ipfs.io';
 
